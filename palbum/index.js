e966eb9290c2f0f3baebd0a16d75c6032f4a33a4
yfidati <fidati15@gmail.com>
2024-12-30 13:28:38 +0100

    port change

--- palbum/index.js
+++ palbum/index.js
@@ -45,7 +45,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start the server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
